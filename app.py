090a1ca12143e327127a9f0cd8167e8038fd8106
SammyJankis <sammyjankis87@gmail.com>
2016-11-19 18:58:34 +0100

    Added app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+#!flask/bin/python
+from flask import Flask, jsonify
+from credentials import *
+import netifaces, time
+
+app = Flask(__name__)
+
+@app.route('/heatcontrol/getstate',methods=['GET'])
+def getState():
+    return jsonify({'state': True})
+
+def is_interface_up(interface):
+    addr = netifaces.ifaddresses(interface)
+    return netifaces.AF_INET in addr
+
+if __name__ == '__main__':
+    while not is_interface_up('wlan0'):
+	time.sleep(10)
+    app.run(host=rasp_host,port=rasp_port,debug=True)
